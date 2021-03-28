08aa5902d0f62b0e5266fb119413717dc8b91e16
Ferotiq <64989416+Ferotiq@users.noreply.github.com>
2021-03-28 01:02:19 -0500

    Delete fero-dc.js

--- src/fero-dc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    Client: require("./Classes/Client.js"),
-    Command: require("./Classes/Command.js"),
-    Argument: require("./Classes/Argument.js"),
-    Event: require("./Classes/Event.js"),
-    Discord: require("discord.js")
-}
